5d227459b7c98307e65ada3dd8ad2b0ad34f545f
Jason Chin <jason.rob.chin@gmail.com>
2019-01-05 13:38:14 -0500

    add verbose tests

--- tests/logTests.cpp
+++ tests/logTests.cpp
@@ -3,11 +3,13 @@
 #include <iostream>
 #include "Log.hpp"
 
-TEST_CASE("debug off") {
+TEST_CASE("debug: off, verbose: off") {
     
     std::cout << "== LOG TEST: DEBUG OFF" << std::endl;
+    std::cout << "== LOG TEST: VERBOSE OFF" << std::endl;
 
     Log::debugMode(false);
+    Log::verboseMode(false);
 
     SECTION("color off") {
         std::cout << "==== COLOR OFF" << std::endl;
@@ -16,6 +18,7 @@ TEST_CASE("debug off") {
         Log::warning("warning");
         Log::error("error");
         Log::debug("debug");
+        Log::verbose("verbose");
     }
 
     SECTION("color on") {
@@ -25,15 +28,18 @@ TEST_CASE("debug off") {
         Log::warning("warning");
         Log::error("error");
         Log::debug("debug");
+        Log::verbose("verbose");
     }
 
 }
 
-TEST_CASE("debug on") {
-
-    std::cout << "== LOG TEST: DEBUG ON" << std::endl;
+TEST_CASE("debug: on, verbose: off") {
     
+    std::cout << "== LOG TEST: DEBUG ON" << std::endl;
+    std::cout << "== LOG TEST: VERBOSE OFF" << std::endl;
+
     Log::debugMode(true);
+    Log::verboseMode(false);
 
     SECTION("color off") {
         std::cout << "==== COLOR OFF" << std::endl;
@@ -42,6 +48,7 @@ TEST_CASE("debug on") {
         Log::warning("warning");
         Log::error("error");
         Log::debug("debug");
+        Log::verbose("verbose");
     }
 
     SECTION("color on") {
@@ -51,6 +58,67 @@ TEST_CASE("debug on") {
         Log::warning("warning");
         Log::error("error");
         Log::debug("debug");
+        Log::verbose("verbose");
+    }
+
+}
+
+TEST_CASE("debug: off, verbose: on") {
+    
+    std::cout << "== LOG TEST: DEBUG OFF" << std::endl;
+    std::cout << "== LOG TEST: VERBOSE ON" << std::endl;
+
+    Log::debugMode(false);
+    Log::verboseMode(true);
+
+    SECTION("color off") {
+        std::cout << "==== COLOR OFF" << std::endl;
+        Log::colors(false);
+        Log::log("log");
+        Log::warning("warning");
+        Log::error("error");
+        Log::debug("debug");
+        Log::verbose("verbose");
+    }
+
+    SECTION("color on") {
+        std::cout << "==== COLOR ON" << std::endl;
+        Log::colors(true);
+        Log::log("log");
+        Log::warning("warning");
+        Log::error("error");
+        Log::debug("debug");
+        Log::verbose("verbose");
+    }
+
+}
+
+TEST_CASE("debug: on, verbose: on") {
+    
+    std::cout << "== LOG TEST: DEBUG ON" << std::endl;
+    std::cout << "== LOG TEST: VERBOSE ON" << std::endl;
+
+    Log::debugMode(true);
+    Log::verboseMode(true);
+
+    SECTION("color off") {
+        std::cout << "==== COLOR OFF" << std::endl;
+        Log::colors(false);
+        Log::log("log");
+        Log::warning("warning");
+        Log::error("error");
+        Log::debug("debug");
+        Log::verbose("verbose");
+    }
+
+    SECTION("color on") {
+        std::cout << "==== COLOR ON" << std::endl;
+        Log::colors(true);
+        Log::log("log");
+        Log::warning("warning");
+        Log::error("error");
+        Log::debug("debug");
+        Log::verbose("verbose");
     }
 
 }
